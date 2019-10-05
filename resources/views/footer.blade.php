b901eccdf16024e81afbef97a7c2d007be1be40f
TRANCONGVU <vndvip10>
2019-10-05 16:06:04 +0700

    uptemplate

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+	<footer class="container-fluid">
+
+	</footer>
+
+	<section class="back-to-top"><div class="back-to-top-button"><i class="fas fa-angle-double-up"></i></div></section>
+	<script type="text/javascript" src="js/backtotop.js"></script>
---
+++ resources/views/pages/trangchu.blade.php
@@ -0,0 +1,11 @@
+
+
+	@extends('master-layout')
+	@section('content')
+
+		<script type="text/javascript" src="lib/jquery.min.js"></script>
+		<script type="text/javascript" src="lib/OwlCarousel2-2.3.4/owl.carousel.min.js"></script>
+		<script type="text/javascript" src="js/doitac-carousel-config.js"></script>
+	</section>
+
+	@endsection
