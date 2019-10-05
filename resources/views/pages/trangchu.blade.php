9508792e5de0bf3bb731c804297775ba4a0137d2
TRANCONGVU <vndvip10>
2019-10-05 20:57:45 +0700

    fixx

--- resources/views/pages/trangchu.blade.php
+++ resources/views/pages/trangchu.blade.php
@@ -1,10 +1,6 @@
 @extends('master-layout')
 @section('content')
 @include('banner')
-    <div style="height : 400px"></div>
-    <script type="text/javascript" src="lib/jquery.min.js"></script>
-    <script type="text/javascript" src="lib/OwlCarousel2-2.3.4/owl.carousel.min.js"></script>
-    <script type="text/javascript" src="js/doitac-carousel-config.js"></script>
-</section>
 
+</section>
 @endsection
