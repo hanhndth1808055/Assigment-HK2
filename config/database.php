ad5293a0154a72489413dff0ead0b4a3f89e5d14
HanhND <hanhndth1808055@fpt.edu.vn>
2019-10-24 20:58:54 +0700

    Fix bugs

--- config/database.php
+++ config/database.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
-
 return [
 
     /*
@@ -17,8 +15,8 @@ return [
     |
     */
 
-//    'default' => env('DB_CONNECTION', 'mysql'),
-    'default' => 'pgsql',
+    'default' => env('DB_CONNECTION', 'mysql'),
+//    'default' => 'pgsql',
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -45,42 +43,42 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-//        'mysql' => [
-//            'driver' => 'mysql',
-//            'url' => env('DATABASE_URL'),
-//            'host' => env('DB_HOST', '127.0.0.1'),
-//            'port' => env('DB_PORT', '3306'),
-//            'database' => env('DB_DATABASE', 'forge'),
-//            'username' => env('DB_USERNAME', 'forge'),
-//            'password' => env('DB_PASSWORD', ''),
-//            'unix_socket' => env('DB_SOCKET', ''),
-//            'charset' => 'utf8mb4',
-//            'collation' => 'utf8mb4_unicode_ci',
-//            'prefix' => '',
-//            'prefix_indexes' => true,
-//            'strict' => true,
-//            'engine' => null,
-//            'options' => extension_loaded('pdo_mysql') ? array_filter([
-//                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-//            ]) : [],
-//        ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => $DATABASE_URL["ec2-174-129-253-125.compute-1.amazonaws.com"],
-            'port' => $DATABASE_URL["5432"],
-            'database' => ltrim($DATABASE_URL["d95rs96njpgg45"], "/"),
-            'username' => $DATABASE_URL["wwxosaycvieriw"],
-            'password' => $DATABASE_URL["b66859b0cac3f79d6fc5f1983ea8399692a0295ff5b7def4795167d573759470"],
-            'charset' => 'utf8',
+        'mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'require',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
 //        'pgsql' => [
 //            'driver' => 'pgsql',
-//            'url' => env('DATABASE_URL'),
+//            'host' => $DATABASE_URL["ec2-174-129-253-125.compute-1.amazonaws.com"],
+//            'port' => $DATABASE_URL["5432"],
+//            'database' => ltrim($DATABASE_URL["d95rs96njpgg45"], "/"),
+//            'username' => $DATABASE_URL["wwxosaycvieriw"],
+//            'password' => $DATABASE_URL["b66859b0cac3f79d6fc5f1983ea8399692a0295ff5b7def4795167d573759470"],
+//            'charset' => 'utf8',
+//            'prefix' => '',
+//            'schema' => 'public',
+//            'sslmode' => 'require',
+//        ],
+
+        'pgsql' => [
+//            'driver' => 'pgsql',
+            'url' => 'postgres://wwxosaycvieriw:b66859b0cac3f79d6fc5f1983ea8399692a0295ff5b7def4795167d573759470@ec2-174-129-253-125.compute-1.amazonaws.com:5432/d95rs96njpgg45',
 //            'host' => env('DB_HOST', '127.0.0.1'),
 //            'port' => env('DB_PORT', '5432'),
 //            'database' => env('DB_DATABASE', 'forge'),
@@ -91,7 +89,7 @@ return [
 //            'prefix_indexes' => true,
 //            'schema' => 'public',
 //            'sslmode' => 'prefer',
-//        ],
+        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
